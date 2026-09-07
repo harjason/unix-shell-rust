56a87982d74f8b6021717cdb17f59c1daa17ecb0
harjason <nosajrarb2@gmail.com>
2026-09-07 19:31:32 +0530

    Add parser skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::io;
 use std::io::Write;
 
-//module definitions
 pub mod parser;
 
-
 fn main() {
-    parser::check();
+
     println!("welcome to my unix shell");
     print!("Enter a username: ");
     let _ = io::stdout().flush(); 
     let mut username: String = String::new();
     let _ = io::stdin().read_line(&mut username);
-
     let pre: String = format!("{}@shell", username.trim_end());
-
+    
     let mut command: String = String::new();
     
     loop{
@@ -23,5 +20,8 @@ fn main() {
         let _ = io::stdout().flush();
         let _ = io::stdin().read_line(&mut command);
         if command.trim() == "exit" {break;}
+        let _parsed_vector = parser::parse(command.trim().to_string());
+        //.trim() returns a immutable reference &str while .to_stirng allocates a new string on the heap and no ownership is moved
+        println!("{:?}" , _parsed_vector)
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,26 @@
-pub fn check(){
-    println!("it works");
+
+#[derive(Debug)]
+struct Command{
+    command_type: String,
+    command_args: Option<String>
+}
+
+pub fn parse(cmd: String) -> Vec<String>{
+    println!("parsing '{}' started", cmd);
+
+    let mut parsed_vector: Vec<String> = Vec::new(); //contains parsed words
+    let mut word: String = String::new();
+    for i in 0..cmd.len(){
+        let c = cmd.chars().nth(i);
+        if c != Some(' '){word.push(c.unwrap_or(' '))}
+        else{
+            parsed_vector.push(word.clone());
+            word.clear();
+        }
+        if i == cmd.len() -1 {
+            parsed_vector.push(word.clone());
+            word.clear();
+        }
+    } 
+   parsed_vector
 }
