c8ad559cf399b8573eb4d13609226248ab3bbe34
harjason <nosajrarb2@gmail.com>
2026-09-07 17:18:26 +0530

    Add username input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::io;
 use std::io::Write;
 
+//module definitions
+pub mod parser;
+
+
 fn main() {
+    parser::check();
     println!("welcome to my unix shell");
+    print!("Enter a username: ");
+    let _ = io::stdout().flush(); 
+    let mut username: String = String::new();
+    let _ = io::stdin().read_line(&mut username);
+
+    let pre: String = format!("{}@shell", username.trim_end());
+
     let mut command: String = String::new();
+    
     loop{
         command.clear();
-        print!("user@computer  ");
-        io::stdout().flush();
+        print!("[{}]$   ",pre);
+        let _ = io::stdout().flush();
         let _ = io::stdin().read_line(&mut command);
         if command.trim() == "exit" {break;}
     }
